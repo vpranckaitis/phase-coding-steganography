2d2b738a060f66e4e47ad2485c255fd8902da106
weras <weras11@gmail.com>
2016-12-17 03:45:00 +0200

    Finally figured out what the problem was. Matlab's native functions were called instead of our own! Seems to be working as previously ATM.

--- +helpers/bits2text.m
+++ +helpers/bits2text.m
@@ -1,8 +1,19 @@
-function [ text ] = bits2text(textBits)
+function [text] = bits2text(textBits)
     % UNTITLED Summary of this function goes here
     %   Detailed explanation goes here
 
     textBitsMatrix = reshape(textBits, 8, round(length(textBits) / 8))';
     textBytes = bi2de(textBitsMatrix);
     text = native2unicode(textBytes)';
+end
+
+function Y = bi2de(X)
+    % UNTITLED Summary of this function goes here
+    %   Detailed explanation goes here
+
+    Y = zeros(size(X, 1), 1);
+    weights = 2 .^ (7 : -1 : 0);
+    for i = 1 : size(X, 1)
+        Y(i) = sum(X(i, :) * weights');
+    end
 end
--- +helpers/text2bits.m
+++ +helpers/text2bits.m
@@ -1,8 +1,18 @@
-function [ textBits ] = text2bits(text)
+function [textBits] = text2bits(text)
     % UNTITLED Summary of this function goes here
     %   Detailed explanation goes here
 
     textBytes = unicode2native(text)';
     textBitsMatrix = de2bi(textBytes);
     textBits = reshape(textBitsMatrix', length(textBitsMatrix(:)), 1);
+end
+
+function Y = de2bi(X)
+    % UNTITLED Summary of this function goes here
+    %   Detailed explanation goes here
+
+    Y = zeros(size(X, 1), 8);
+    for i = 1 : size(X, 1)
+        Y(i, :) = bitget(X(i), 8 : -1 : 1);
+    end
 end
